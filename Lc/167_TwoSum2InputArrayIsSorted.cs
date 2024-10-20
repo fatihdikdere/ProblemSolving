1c0dbd56532727ca4e1e1cf895d6feae1c0144ec
fatihd <fatihdikdere@gmail.com>
2024-10-20 22:13:00 +0300

    167_TwoSum2InputArrayIsSorted was added

---
+++ Lc/167_TwoSum2InputArrayIsSorted.cs
@@ -0,0 +1,32 @@
+﻿namespace Lc;
+public class _167_TwoSum2InputArrayIsSorted
+{
+    public int[] TwoSum(int[] numbers, int target)
+    {
+        var left = 0;
+        var right = numbers.Length - 1;
+
+        while (left < right)
+        {
+            var currentSum = numbers[left] + numbers[right];
+
+            if (currentSum == target)
+            {
+                return [left + 1, right + 1];
+            }
+
+            if (currentSum > target)
+            {
+                right -= 1;
+                continue;
+            }
+
+            if (currentSum < target)
+            {
+                left += 1;
+            }
+        }
+
+        return [];
+    }
+}
