f5d9cd251c9eccdee7e55654a061c6b441687b45
fatihd <fatihdikdere@gmail.com>
2024-10-20 17:10:38 +0300

    1_TwoSum was added

---
+++ Lc/1_TwoSum.cs
@@ -0,0 +1,22 @@
+﻿namespace Lc;
+public class _1_TwoSum
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var temp = new Dictionary<int, int>();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var diff = target - nums[i];
+
+            if (temp.TryGetValue(diff, out int value))
+            {
+                return [value, i];
+            }
+
+            temp[nums[i]] = i;
+        }
+
+        return [];
+    }
+}
