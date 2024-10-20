3b35b6d870dfc3fb9c431af29dd05c4771d767aa
fatihd <fatihdikdere@gmail.com>
2024-10-20 22:10:52 +0300

    303_RangeSumQueryImmutable was added

---
+++ Lc/303_RangeSumQueryImmutable.cs
@@ -0,0 +1,26 @@
+﻿namespace Lc;
+public class _303_RangeSumQueryImmutable
+{
+    private readonly List<int> prefixSum = [];
+    public _303_RangeSumQueryImmutable(int[] nums)
+    {
+        var current = 0;
+
+        foreach (var num in nums)
+        {
+            current += num;
+
+            prefixSum.Add(current);
+        }
+    }
+    public int SumRange(int left, int right)
+    {
+        var result = prefixSum[right] - (
+            left > 0
+                ? prefixSum[left - 1]
+                : 0
+        );
+
+        return result;
+    }
+}
