95318b259e940956bb6842d3261b1b874d0ee48b
fatihd <fatihdikdere@gmail.com>
2024-10-21 23:23:43 +0300

    206_ReverseLinkedList was added

---
+++ Lc/206_ReverseLinkedList.cs
@@ -0,0 +1,30 @@
+﻿namespace Lc;
+public class _206_ReverseLinkedList
+{
+    public ListNode ReverseList(ListNode head)
+    {
+        ListNode previous = null;
+        ListNode current = head;
+
+        while (current != null)
+        {
+            var nextNode = current.next;
+
+            current.next = previous;
+            previous = current;
+            current = nextNode;
+        }
+
+        return previous;
+    }
+}
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
