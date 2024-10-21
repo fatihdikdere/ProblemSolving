976b4caf5089bef64ea804515048a11a92b1dfbc
fatihd <fatihdikdere@gmail.com>
2024-10-21 23:26:21 +0300

    33_SearchInRotatedSortedArray was added

---
+++ Lc/33_SearchInRotatedSortedArray.cs
@@ -0,0 +1,44 @@
+﻿namespace Lc;
+public class _33_SearchInRotatedSortedArray
+{
+    public int Search(int[] nums, int target)
+    {
+        var left = 0;
+        var right = nums.Length - 1;
+
+        while (left <= right)
+        {
+            int middle = (left + right) / 2;
+
+            if (nums[middle] == target)
+            {
+                return middle;
+            }
+
+            if (nums[left] <= nums[middle])
+            {
+                if (target > nums[middle] || target < nums[left])
+                {
+                    left = middle + 1;
+                }
+                else
+                {
+                    right = middle - 1;
+                }
+            }
+            else
+            {
+                if (target < nums[middle] || target > nums[right])
+                {
+                    right = middle - 1;
+                }
+                else
+                {
+                    left = middle + 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
