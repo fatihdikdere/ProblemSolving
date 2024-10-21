80e4d3f63ce017ebab9ac3aaa0b4855aee92f006
fatihd <fatihdikdere@gmail.com>
2024-10-21 23:20:55 +0300

    643_MaximumAverageSubarray1 was added

---
+++ Lc/643_MaximumAverageSubarray1.cs
@@ -0,0 +1,19 @@
+﻿namespace Lc;
+public class _643_MaximumAverageSubarray1
+{
+    public double FindMaxAverage(int[] nums, int k)
+    {
+        var currentSum = nums.Take(k).Sum();
+        var maxSum = currentSum;
+
+        for (var i = k; i < nums.Length; i++)
+        {
+            currentSum += nums[i] - nums[i - k];
+            maxSum = Math.Max(maxSum, currentSum);
+        }
+
+        var result = (double)maxSum / k;
+
+        return result;
+    }
+}
