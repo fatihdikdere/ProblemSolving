8b02d531878efbfea5b38afb32ac96a017a2ca2f
fatihd <fatihdikdere@gmail.com>
2024-10-21 23:22:09 +0300

    202_HappyNumber was added

---
+++ Lc/202_HappyNumber.cs
@@ -0,0 +1,35 @@
+﻿namespace Lc;
+public class _202_HappyNumber
+{
+    public bool IsHappy(int n)
+    {
+        var visit = new HashSet<int>();
+
+        while (!visit.Contains(n))
+        {
+            visit.Add(n);
+
+            n = GetSumOfSquares(n);
+
+            if (n == 1)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+    private int GetSumOfSquares(int n)
+    {
+        var result = 0;
+
+        while (n > 0)
+        {
+            var digit = n % 10;
+            result += digit * digit;
+            n /= 10;
+        }
+
+        return result;
+    }
+}
