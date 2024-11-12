b36a6a2ba3e52cc2436b31b3defb65ead478dc67
fatihd <fatihdikdere@gmail.com>
2024-11-12 23:54:00 +0300

    217_ContainsDuplicate  was added

---
+++ Lc/217_ContainsDuplicate.cs
@@ -0,0 +1,20 @@
+﻿namespace Lc;
+public class _217_ContainsDuplicate
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        var numsHash = new HashSet<int>();
+
+        foreach (var num in nums)
+        {
+            if (numsHash.Contains(num))
+            {
+                return true;
+            }
+
+            numsHash.Add(num);
+        }
+
+        return false;
+    }
+}
