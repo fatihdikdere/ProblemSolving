f295f640f8e4758789b6bb4362c28e0189317870
fatihd <fatihdikdere@gmail.com>
2024-11-13 22:33:29 +0300

    412_FizzBuzz was added

---
+++ Lc/412_FizzBuzz.cs
@@ -0,0 +1,36 @@
+﻿namespace Lc;
+public class _412_FizzBuzz
+{
+    public IList<string> FizzBuzz(int n)
+    {
+        var result = new List<string>();
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 15 == 0)
+            {
+                result.Add("FizzBuzz");
+
+                continue;
+            }
+
+            if (i % 5 == 0)
+            {
+                result.Add("Buzz");
+
+                continue;
+            }
+
+            if (i % 3 == 0)
+            {
+                result.Add("Fizz");
+
+                continue;
+            }
+
+            result.Add(i.ToString());
+        }
+
+        return result;
+    }
+}
