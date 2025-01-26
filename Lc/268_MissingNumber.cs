4aebf54a6cfa49d2461a465985dfb5fc09dedcb7
fatihd <fatihdikdere@gmail.com>
2025-01-26 22:27:56 +0300

    268_MissingNumber was added

---
+++ Lc/268_MissingNumber.cs
@@ -0,0 +1,16 @@
+﻿namespace Lc;
+public class _268_MissingNumber
+{
+    public int MissingNumber(int[] nums)
+    {
+        var result = 0;
+        var resultExpected = (nums.Length * (nums.Length + 1)) / 2;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            result += nums[i];
+        }
+
+        return resultExpected - result;
+    }
+}
