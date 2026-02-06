762bcbc325ba7c81339db13fcaf8df4a1f4f0a33
fatihd <fatihdikdere@gmail.com>
2026-02-06 09:26:03 +0300

    test

--- Lc/Program.cs
+++ Lc/Program.cs
@@ -1,2 +1,14 @@
 ﻿Console.WriteLine("fatihd");
+
+
+using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+{
+	byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes("fatihd");
+	byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+	Console.WriteLine(Convert.ToHexString(hashBytes));
+
+	Console.WriteLine("Hello World");
+}
+
 Console.ReadKey();
