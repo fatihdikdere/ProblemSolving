89e6fee41b981d6e6852e8b76352f0f15c839489
fatihd <fatihdikdere@gmail.com>
2026-02-06 09:34:34 +0300

    test2

--- Lc/Program.cs
+++ Lc/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.WriteLine("fatihd");
+﻿using System;
+using System.Data.SqlClient;
+
+Console.WriteLine("fatihd");
 
 
 using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
@@ -9,6 +12,23 @@ using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.C
 	Console.WriteLine(Convert.ToHexString(hashBytes));
 
 	Console.WriteLine("Hello World");
+}
+
+var connectionString = "Server=.;Database=test;User Id=sa;Password=123456;";
+
+using var connection = new SqlConnection(connectionString);
+
+connection.Open();
+
+var query = "SELECT * FROM Users WHERE Username = '" + username + "'";
+
+var command = new SqlCommand(query, connection);
+
+var reader = command.ExecuteReader();
+
+while (reader.Read())
+{
+	Console.WriteLine(reader["Username"]);
 }
 
 Console.ReadKey();
